382c49cbc72ca51d755be61c2b8162fe0fcc519c
LxHTT <lxhtt@mcsl.com.cn>
2024-05-01 18:13:52 +0800

    animation: use inkore wpf frame

---
+++ MCServerLauncher.UI/View/CreateInstancePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Windows;
+using Page = System.Windows.Controls.Page;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System;
+using iNKORE.UI.WPF.Modern.Controls;
+using MCServerLauncher.UI.View.CreateInstanceProvider;
+
+namespace MCServerLauncher.UI.View{
+    /// <summary>
+    /// CreateInstancePage.xaml 的交互逻辑
+    /// </summary>
+    
+    public partial class CreateInstancePage : Page
+    {
+        public PreCreateInstance preCreateInstance = new PreCreateInstance();
+        public CreateInstancePage()
+        {
+            InitializeComponent();
+            CurrentCreateInstance.Content = preCreateInstance;
+        }
+        public UserControl NewMinecraftJavaServerPage()
+        {
+            return new CreateMinecraftJavaInstanceProvider();
+        }
+        public UserControl NewMinecraftForgeServerPage()
+        {
+            return new CreateMinecraftForgeInstanceProvider();
+        }
+        public UserControl NewMinecraftBedrockServerPage()
+        {
+            return new CreateMinecraftBedrockInstanceProvider();
+        }
+        public UserControl NewOtherExecutablePage()
+        {
+            return new CreateOtherExecutableInstanceProvider();
+        }
+    }
+}
