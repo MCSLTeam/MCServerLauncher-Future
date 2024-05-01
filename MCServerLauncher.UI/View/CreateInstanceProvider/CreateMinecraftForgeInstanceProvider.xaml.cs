810dcc19e83c4c15688e2840f0ec3123a1443d84
LxHTT <lxhtt@mcsl.com.cn>
2024-05-01 18:16:28 +0800

    feat: Minecraft Forge server creation

---
+++ MCServerLauncher.UI/View/CreateInstanceProvider/CreateMinecraftForgeInstanceProvider.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MCServerLauncher.UI.Tools;
+using MCServerLauncher.UI.View.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MCServerLauncher.UI.View.CreateInstanceProvider
+{
+    /// <summary>
+    /// CreateMinecraftForgeInstanceProvider.xaml 的交互逻辑
+    /// </summary>
+    public partial class CreateMinecraftForgeInstanceProvider : UserControl
+    {
+        public CreateMinecraftForgeInstanceProvider()
+        {
+            InitializeComponent();
+        }
+        private void GoPreCreateInstance(object sender, RoutedEventArgs e)
+        {
+            var parent = this.TryFindParent<CreateInstancePage>();
+            parent.CurrentCreateInstance.GoBack();
+        }
+        private void FinishSetup(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+        private void AddJVMArgument(object sender, RoutedEventArgs e)
+        {
+            JVMArgumentListView.Items.Add(new JVMArgumentItem());
+        }
+    }
+}
