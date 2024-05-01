8b7368688605d54bc97f7625fcb82db238e26457
LxHTT <lxhtt@mcsl.com.cn>
2024-05-01 18:17:12 +0800

    ?

---
+++ MCServerLauncher.UI/View/ResDownloadPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Page = System.Windows.Controls.Page;
+
+namespace MCServerLauncher.UI.View
+{
+    /// <summary>
+    /// ResDownloadPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ResDownloadPage : Page
+    {
+        public ResDownloadPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MCServerLauncher.UI/View/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MCServerLauncher.UI.View
+{
+    /// <summary>
+    /// SettingsPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
