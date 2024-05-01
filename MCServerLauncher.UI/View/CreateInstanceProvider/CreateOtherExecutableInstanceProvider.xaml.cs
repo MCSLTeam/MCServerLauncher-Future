50ba4961fdb34d230173065a8ee9d2e1f7f004f6
LxHTT <lxhtt@mcsl.com.cn>
2024-05-01 18:17:46 +0800

    feat: other executable type creation

---
+++ MCServerLauncher.UI/View/CreateInstanceProvider/CreateOtherExecutableInstanceProvider.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MCServerLauncher.UI.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MCServerLauncher.UI.View.CreateInstanceProvider
+{
+    /// <summary>
+    /// CreateOtherExecutableInstanceProvider.xaml 的交互逻辑
+    /// </summary>
+    public partial class CreateOtherExecutableInstanceProvider : UserControl
+    {
+        public CreateOtherExecutableInstanceProvider()
+        {
+            InitializeComponent();
+        }
+        private void GoPreCreateInstance(object sender, RoutedEventArgs e)
+        {
+            var parent = this.TryFindParent<CreateInstancePage>();
+            parent.CurrentCreateInstance.GoBack();
+        }
+        private void FinishSetup(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+    }
+}
