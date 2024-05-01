b3ac9cd1cb9a4065d026701f0019770330e4ee35
LxHTT <lxhtt@mcsl.com.cn>
2024-05-01 18:08:04 +0800

    feat: app resource

---
+++ MCServerLauncher.UI/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using MessageBox = iNKORE.UI.WPF.Modern.Controls.MessageBox;
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+
+namespace MCServerLauncher.UI
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // 添加崩溃处理事件
+            DispatcherUnhandledException += (s, e) =>
+            {
+                MessageBox.Show("MCServerLauncher 发生了未经处理的异常：\n\n" + e.Exception.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true; // 设置为已处理，阻止应用程序崩溃
+            };
+        }
+
+        //以创建Mutex的方式防止同目录多开，避免奇奇怪怪的文件占用错误
+        private Mutex _mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool createNew;
+            _mutex = new Mutex(true, Assembly.GetExecutingAssembly().GetName().Name, out createNew);
+            if (!createNew)
+            {
+                MessageBox.Show("s 不支持重复运行。", "提示", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+
+                Environment.Exit(0);
+            }
+            base.OnStartup(e);
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_mutex != null)
+            {
+                _mutex.ReleaseMutex();
+            }
+            base.OnExit(e);
+        }
+    }
+}
+
