5f24c887daf3364feb6c9e4b100b91f9d91cba67
LxHTT <lxhtt@mcsl.com.cn>
2024-05-01 18:15:24 +0800

    reduce coupling

---
+++ MCServerLauncher.UI/Tools/FindParent.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Media;
+using System.Windows;
+
+namespace MCServerLauncher.UI.Tools
+{
+    internal static class VisualTreeExtensions
+    {
+        public static T TryFindParent<T>(this DependencyObject child) where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(child);
+            while (parent != null)
+            {
+                if (parent is T parentType)
+                {
+                    return parentType;
+                }
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+            return null;
+        }
+    }
+}
---
+++ MCServerLauncher.UI/View/CreateInstanceProvider/PreCreateInstance.xaml.cs
@@ -0,0 +1,56 @@
+﻿using static MCServerLauncher.UI.Tools.VisualTreeExtensions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Text.RegularExpressions;
+using System;
+
+namespace MCServerLauncher.UI.View.CreateInstanceProvider
+{
+    /// <summary>
+    /// PreCreateInstance.xaml 的交互逻辑
+    /// </summary>
+    public partial class PreCreateInstance : UserControl
+    {
+        public string CreatingInstanceType = "PreCreating";
+        public PreCreateInstance()
+        {
+            InitializeComponent();
+        }
+        public void SelectNewInstanceType(object Sender, MouseButtonEventArgs MouseArg)
+        {
+            Console.WriteLine(((Border)Sender).Name);
+            CreatingInstanceType = ((Border)Sender).Name;
+            Console.WriteLine(CreatingInstanceType);
+            SelectNewInstanceTypeContinueBtn.IsEnabled = true;
+        }
+        public void GoCreateInstance(object Sender, RoutedEventArgs e)
+        {
+            if (CreatingInstanceType == "PreCreating")
+            {
+                return;
+            }
+            else
+            {
+                var parent = this.TryFindParent<CreateInstancePage>();
+                switch (CreatingInstanceType)
+                {
+                    case "MinecraftJavaServer":
+                        parent.CurrentCreateInstance.Content = parent.NewMinecraftJavaServerPage();
+                        break;
+                    case "MinecraftForgeServer":
+                        parent.CurrentCreateInstance.Content = parent.NewMinecraftForgeServerPage();
+                        break;
+                    case "MinecraftBedrockServer":
+                        parent.CurrentCreateInstance.Content = parent.NewMinecraftBedrockServerPage();
+                        break;
+                    case "OtherExecutable":
+                        parent.CurrentCreateInstance.Content = parent.NewOtherExecutablePage();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }   
+    }
+}
