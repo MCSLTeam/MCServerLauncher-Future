232587881babb11059a5c82f523d991ac4a0a755
LxHTT <lxhtt@mcsl.com.cn>
2024-05-01 18:14:15 +0800

    ?

---
+++ MCServerLauncher.UI/View/HelpPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace MCServerLauncher.UI.View
+
+{
+    /// <summary>
+    /// HelpPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class HelpPage : Page
+    {
+        public HelpPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
