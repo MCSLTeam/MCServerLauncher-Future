6a7cc11ad1df5cf032427fd8189d564710d44409
LxHTT <lxhtt@mcsl.com.cn>
2024-05-04 23:18:07 +0800

    [daemon] feat: config model

---
+++ MCServerLauncher.Daemon/InstanceManager/InstanceConfigModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+
+public class MinecraftJavaInstanceConfig
+{
+    public string CoreName { get; set; }
+    public string JavaRuntimePath { get; set; }
+    public int JvmMinimumMemory { get; set; }
+    public int JvmMaximumMemory { get; set; }
+    public List<string> JvmArguments { get; set; }
+    public string InstanceName { get; set; }
+
+    public string GetSerializedJsonString()
+    {
+        return JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+}
