635286efd2aae60770dd59d401c8974c8cf5c40f
LxHTT <lxhtt@mcsl.com.cn>
2024-07-15 13:00:56 +0800

    feat: range setting card

---
+++ MCServerLauncher.UI/View/Components/RangeSettingCard.xaml.cs
@@ -0,0 +1,59 @@
+﻿using iNKORE.UI.WPF.Modern.Common.IconKeys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MCServerLauncher.UI.View.Components
+{
+    /// <summary>
+    /// RangeSettingCard.xaml 的交互逻辑
+    /// </summary>
+    public partial class RangeSettingCard : UserControl
+    {
+        public RangeSettingCard()
+        {
+            InitializeComponent();
+        }
+        public string Title
+        {
+            get => SettingTitle.Text;
+            set => SettingTitle.Text = value;
+        }
+        public string Description
+        {
+            get => SettingDescription.Text;
+            set => SettingDescription.Text = value;
+        }
+        public int MinValue
+        {
+            get => (int)SettingSlider.Minimum;
+            set => SettingSlider.Minimum = value;
+        }
+        public int MaxValue
+        {
+            get => (int)SettingSlider.Maximum;
+            set => SettingSlider.Maximum = value;
+        }
+        public int Value
+        {
+            get => (int)SettingSlider.Value;
+            set => SettingSlider.Value = value;
+        }
+        public FontIconData? Icon
+        {
+            get => SettingIcon.Icon;
+            set => SettingIcon.Icon = value;
+        }
+    }
+}
