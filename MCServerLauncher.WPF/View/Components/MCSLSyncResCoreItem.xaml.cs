d99a70718fa0e9922107cd1f7ac582a78a763dee
LxHTT <lxhtt@mcsl.com.cn>
2024-07-19 15:24:36 +0800

    feat: MCSL-Sync download ui

---
+++ MCServerLauncher.WPF/View/Components/MCSLSyncResCoreItem.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Collections.Generic;
+
+namespace MCServerLauncher.WPF.View.Components
+{
+    /// <summary>
+    /// MCSLSyncResCoreItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class MCSLSyncResCoreItem : UserControl
+    {
+        public MCSLSyncResCoreItem()
+        {
+            InitializeComponent();
+        }
+        public string CoreName
+        {
+            get => CoreNameReplacer.Text;
+            set => CoreNameReplacer.Text = value;
+        }
+    }
+}
