28f4072cac96853bd347a74a8f32e8609a0cbb7b
LxHTT <lxhtt@mcsl.com.cn>
2024-07-23 00:11:47 +0800

    feat: board player list user control

---
+++ MCServerLauncher.WPF.Console/View/Components/MinecraftBedrockInstancePlayerList.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace MCServerLauncher.WPF.Console.View.Components
+{
+    /// <summary>
+    /// MinecraftBedrockInstancePlayerList.xaml 的交互逻辑
+    /// </summary>
+    public partial class MinecraftBedrockInstancePlayerList : UserControl
+    {
+        public MinecraftBedrockInstancePlayerList()
+        {
+            InitializeComponent();
+        }
+        public string PlayerList
+        {
+            get => string.Join(",", PlayerListView.Items.ToString());
+            set
+            {
+                PlayerListView.Items.Clear();
+                string[] CurrentPlayers = value.Split(',');
+                foreach (string Player in CurrentPlayers)
+                {
+                    string[] PlayerInfo = Player.Split('@');
+                    PlayerListView.Items.Add(new PlayerItem() { PlayerName = PlayerInfo[0], PlayerIP = PlayerInfo[1] });
+                }
+            }
+        }
+    }
+}
---
+++ MCServerLauncher.WPF.Console/View/Components/MinecraftJavaInstancePlayerList.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace MCServerLauncher.WPF.Console.View.Components
+{
+    /// <summary>
+    /// MinecraftJavaInstancePlayerList.xaml 的交互逻辑
+    /// </summary>
+    public partial class MinecraftJavaInstancePlayerList : UserControl
+    {
+        public MinecraftJavaInstancePlayerList()
+        {
+            InitializeComponent();
+        }
+        public string PlayerList
+        {
+            get => string.Join(",", PlayerListView.Items.ToString());
+            set
+            {
+                PlayerListView.Items.Clear();
+                string[] CurrentPlayers = value.Split(',');
+                foreach (string Player in CurrentPlayers)
+                {
+                    string[] PlayerInfo = Player.Split('@');
+                    PlayerListView.Items.Add(new PlayerItem() { PlayerName = PlayerInfo[0], PlayerIP = PlayerInfo[1] });
+                }
+            }
+        }
+    }
+}
