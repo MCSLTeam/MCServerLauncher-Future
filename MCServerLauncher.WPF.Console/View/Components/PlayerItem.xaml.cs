eacd31f28800348ff19227c292ebd608388dec82
LxHTT <lxhtt@mcsl.com.cn>
2024-07-23 00:11:54 +0800

    feat: board player item user control

---
+++ MCServerLauncher.WPF.Console/View/Components/PlayerItem.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+
+namespace MCServerLauncher.WPF.Console.View.Components
+{
+    /// <summary>
+    /// PlayerItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class PlayerItem : UserControl
+    {
+        public PlayerItem()
+        {
+            InitializeComponent();
+        }
+        public string PlayerName
+        {
+            get => PlayerNameTextBlock.Text;
+            set => PlayerNameTextBlock.Text = value;
+        }
+        public string PlayerIP
+        {
+            get => IPTextBox.Text;
+            set => IPTextBox.Text = value;
+        }
+    }
+}
