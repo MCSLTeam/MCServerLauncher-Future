1e239c3cbf259d57167c4789e114df03c83e440d
LxHTT <lxhtt@mcsl.com.cn>
2024-07-24 15:19:38 +0800

    feat: determine slide animation direction

--- MCServerLauncher.WPF.Console/MainWindow.xaml.cs
+++ MCServerLauncher.WPF.Console/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using iNKORE.UI.WPF.Modern.Media.Animation;
 using MCServerLauncher.WPF.Console.View;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 using Page = System.Windows.Controls.Page;
 
@@ -14,6 +15,7 @@ namespace MCServerLauncher.WPF.Console
     {
         private Page Board = new BoardPage();
         private Page Command = new CommandPage();
+        private Page FileManager = new FileManagerPage();
         public MainWindow()
         {
             InitializeComponent();
@@ -22,7 +24,7 @@ namespace MCServerLauncher.WPF.Console
 
         private void NavigationTriggered(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
-            if (args.IsSettingsInvoked == true)
+            if (args.IsSettingsInvoked)
             {
                 NavigateTo(typeof(int), args.RecommendedNavigationTransitionInfo);
             }
@@ -40,13 +42,26 @@ namespace MCServerLauncher.WPF.Console
                 switch (navPageType)
                 {
                     case Type t when t == typeof(BoardPage):
-                        CurrentPage.Navigate(Board, new SlideNavigationTransitionInfo());
+                        CurrentPage.Navigate(Board, new SlideNavigationTransitionInfo { Effect = DetermineSlideDirection(t) });
                         break;
                     case Type t when t == typeof(CommandPage):
-                        CurrentPage.Navigate(Command, new SlideNavigationTransitionInfo());
+                        CurrentPage.Navigate(Command, new SlideNavigationTransitionInfo { Effect = DetermineSlideDirection(t) });
+                        break;
+                    case Type t when t == typeof(FileManagerPage):
+                        CurrentPage.Navigate(FileManager, new SlideNavigationTransitionInfo { Effect = DetermineSlideDirection(t) });
                         break;
                 }
             }
+        }
+        private SlideNavigationTransitionEffect DetermineSlideDirection(Type navPageType)
+        {
+            Page current = (Page)CurrentPage.Content;
+            List<Type> pages = new List<Type> { typeof(BoardPage), typeof(CommandPage), typeof(FileManagerPage) };
+            if (pages.IndexOf(current.GetType()) < pages.IndexOf(navPageType))
+            {
+                return SlideNavigationTransitionEffect.FromRight;
+            }
+            return SlideNavigationTransitionEffect.FromLeft;
         }
     }
 }
