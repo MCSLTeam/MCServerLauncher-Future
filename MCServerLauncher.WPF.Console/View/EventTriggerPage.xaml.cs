8501f271fdd4adeaf99182580916da3b49a8b82a
LxHTT <lxhtt@mcsl.com.cn>
2024-07-25 21:52:18 +0800

    feat: event trigger ui

---
+++ MCServerLauncher.WPF.Console/View/EventTriggerPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MCServerLauncher.WPF.Console.View
+{
+    /// <summary>
+    /// EventTriggerPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class EventTriggerPage : Page
+    {
+        public EventTriggerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
