abd708e605d40020b0ef72541df063d90ae72194
LxHTT <lxhtt@mcsl.com.cn>
2024-07-26 20:52:30 +0800

    feat: component list

---
+++ MCServerLauncher.WPF.Console/View/Components/InstanceComponentItem.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MCServerLauncher.WPF.Console.View.Components
+{
+    /// <summary>
+    ///     InstanceComponentItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class InstanceComponentItem
+    {
+        public InstanceComponentItem()
+        {
+            InitializeComponent();
+        }
+        public string ComponentFileName
+        {
+            get => ComponentFileNameTextBlock.Text;
+            set => ComponentFileNameTextBlock.Text = value;
+        }
+    }
+}
