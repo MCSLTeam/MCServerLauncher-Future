739347d6671392e29cfb7df78b0f0e601d7c31f2
LxHTT <lxhtt@mcsl.com.cn>
2024-07-26 15:56:47 +0800

    feat: ComponentManagerPage

---
+++ MCServerLauncher.WPF.Console/View/ComponentManagerPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MCServerLauncher.WPF.Console.View
+{
+    /// <summary>
+    ///     ComponentManagerPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ComponentManagerPage
+    {
+        public ComponentManagerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
