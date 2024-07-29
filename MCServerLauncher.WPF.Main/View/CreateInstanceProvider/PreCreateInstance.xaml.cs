2edfc765c4979f7394d4e1766199587e25ecb703
LxHTT <lxhtt@mcsl.com.cn>
2024-07-29 21:55:03 +0800

    ref: useless using

--- MCServerLauncher.WPF.Main/View/CreateInstanceProvider/PreCreateInstance.xaml.cs
+++ MCServerLauncher.WPF.Main/View/CreateInstanceProvider/PreCreateInstance.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using iNKORE.UI.WPF.Modern.Media.Animation;
 using static MCServerLauncher.WPF.Main.Helpers.VisualTreeExtensions;
 
