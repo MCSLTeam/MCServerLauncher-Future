ef9640024b8b16264895f8bced525ef668d3d668
LxHTT <lxhtt@mcsl.com.cn>
2024-08-04 05:44:37 +0800

    feat: notification test

--- MCServerLauncher.WPF/View/NotificationCenterPage.xaml.cs
+++ MCServerLauncher.WPF/View/NotificationCenterPage.xaml.cs
@@ -1,15 +1,36 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using iNKORE.UI.WPF.Modern.Controls;
 
 namespace MCServerLauncher.WPF.View
 {
     /// <summary>
-    /// NotificationCenterPage.xaml 的交互逻辑
+    ///     NotificationCenterPage.xaml 的交互逻辑
     /// </summary>
     public partial class NotificationCenterPage
     {
         public NotificationCenterPage()
         {
             InitializeComponent();
+        }
+        public void PushNotification(string title, string message, bool isClosable, InfoBarSeverity severity)
+        {
+            NotificationContainer.Children.Insert(
+                index: 0,
+                element: new InfoBar
+                    {
+                        Title = title,
+                        Message = message,
+                        Severity = severity,
+                        IsClosable = isClosable,
+                        IsOpen = true
+                    }
+                );
+
+        }
+
+        private void TestNotification(object sender, RoutedEventArgs e)
+        {
+            PushNotification(title: "Test", message: "Test", isClosable: true, severity: InfoBarSeverity.Success);
         }
     }
 }
