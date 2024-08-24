e07353ae293aab537cf01f73a4d26aaaef817b0e
LxHTT <lxhtt@mcsl.com.cn>
2024-08-24 19:36:03 +0800

    [wpf] fix: locale code

--- MCServerLauncher.WPF/Utils.cs
+++ MCServerLauncher.WPF/Utils.cs
@@ -232,7 +232,7 @@ namespace MCServerLauncher.WPF.Helpers
                     App = new AppSettings
                     {
                         Theme = "auto",
-                        Language = "zh-CN",
+                        Language = "zh-Hans",
                         FollowStartup = false,
                         AutoCheckUpdate = true,
                         IsCertImported = false,
