1eedaceced2dabcaf4d94c122e4e7c5c989f877a
LxHTT <lxhtt@mcsl.com.cn>
2024-08-25 18:19:47 +0800

    [wpf] feat: interface for res download providers

---
+++ MCServerLauncher.WPF/View/ResDownloadProvider/IResDownloadProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace MCServerLauncher.WPF.View.ResDownloadProvider
+{
+    internal interface IResDownloadProvider
+    {
+        string ResProviderName { get; }
+        Task<bool> Refresh();
+    }
+}
