06ff8b5896d6e944448d61749bdedce0afa67687
LxHTT <lxhtt@mcsl.com.cn>
2024-08-31 17:52:40 +0800

    [wpf] feat: add daemon connection

--- MCServerLauncher.WPF/View/Components/DaemonManager/DaemonBorder.xaml.cs
+++ MCServerLauncher.WPF/View/Components/DaemonManager/DaemonBorder.xaml.cs
@@ -43,16 +43,38 @@ namespace MCServerLauncher.WPF.View.Components.DaemonManager
         {
             if (d is not DaemonBorder control) return;
             if (e.NewValue is not string status) return;
-            bool realStatus = status switch
+            control.StatusLine.Content = status switch
             {
-                "true" => true,
-                "false" => false,
+                "ok" => LanguageManager.Localize["Status_OK"],
+                "err" => LanguageManager.Localize["Status_Error"],
+                "ing" => LanguageManager.Localize["Connecting"],
                 _ => throw new System.NotImplementedException(),
             };
-            control.StatusLine.Content = realStatus ? LanguageManager.Localize["Status_OK"] : LanguageManager.Localize["Status_Error"];
-            control.StatusLine.Icon = realStatus ? SegoeFluentIcons.Accept : SegoeFluentIcons.Error;
-            control.ConnectionControlLine.Content = realStatus ? LanguageManager.Localize["Disconnect"] : LanguageManager.Localize["Retry"];
-            control.ConnectionControlLine.Icon = realStatus ? SegoeFluentIcons.DisconnectDrive : SegoeFluentIcons.Refresh;
+            control.StatusLine.Icon = status switch
+            {
+                "ok" => SegoeFluentIcons.Accept,
+                "err" => SegoeFluentIcons.Error,
+                "ing" => SegoeFluentIcons.HangUp,
+                _ => throw new System.NotImplementedException(),
+            }; 
+            control.ConnectionControlLine.Content = status switch
+            {
+                "ok" => LanguageManager.Localize["Disconnect"],
+                "err" => LanguageManager.Localize["Retry"],
+                "ing" => LanguageManager.Localize["Retry"],
+                _ => throw new System.NotImplementedException(),
+            };
+            //control.ConnectionEditButton.IsEnabled = status != "ing";
+            control.ConnectionControlButton.IsEnabled = status != "ing";
+            control.ConnectionControlLine.Icon = status switch
+            {
+                "ok" => SegoeFluentIcons.DisconnectDrive,
+                "err" => SegoeFluentIcons.Refresh,
+                "ing" => SegoeFluentIcons.Refresh,
+                _ => throw new System.NotImplementedException(),
+            };
         }
+
+        public string JWT { get; set; }
     }
 }
--- MCServerLauncher.WPF/View/FirstSetupHelper/DaemonSetupPage.xaml.cs
+++ MCServerLauncher.WPF/View/FirstSetupHelper/DaemonSetupPage.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using iNKORE.UI.WPF.Modern.Controls;
 using MCServerLauncher.WPF.Modules;
+using MCServerLauncher.WPF.View.Components.DaemonManager;
 using System;
+using System.Net;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 using Page = System.Windows.Controls.Page;
 
 namespace MCServerLauncher.WPF.View.FirstSetupHelper
@@ -48,6 +52,69 @@ namespace MCServerLauncher.WPF.View.FirstSetupHelper
                 // ignored
             }
         }
-        
+
+        private Task<(ContentDialog, NewDaemonConnectionInput)> ConstructConnectDaemonDialog(string address = "", string jwt = "")
+        {
+            NewDaemonConnectionInput newDaemonConnectionInput = new();
+            newDaemonConnectionInput.wsEdit.Text = address;
+            newDaemonConnectionInput.jwtEdit.Password = jwt;
+            ContentDialog dialog = new()
+            {
+                Title = LanguageManager.Localize["ConnectDaemon"],
+                PrimaryButtonText = LanguageManager.Localize["Connect"],
+                SecondaryButtonText = LanguageManager.Localize["Cancel"],
+                DefaultButton = ContentDialogButton.Primary,
+                FullSizeDesired = false,
+                Content = newDaemonConnectionInput
+            };
+            return Task.FromResult<(ContentDialog, NewDaemonConnectionInput)>((dialog, newDaemonConnectionInput));
+        }
+
+        private async void AddDaemonConnection(object sender, RoutedEventArgs e)
+        {
+            (ContentDialog dialog, NewDaemonConnectionInput newDaemonConnectionInput) = await ConstructConnectDaemonDialog();
+            dialog.PrimaryButtonClick += (o, args) => TryConnectDaemon(
+                address: newDaemonConnectionInput.wsEdit.Text,
+                jwt: newDaemonConnectionInput.jwtEdit.Password
+            );
+            try
+            {
+                await dialog.ShowAsync();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        private async Task<RoutedEventHandler> EditDaemonConnection(object sender, RoutedEventArgs e, string address, string jwt, DaemonBorder daemon)
+        {
+            (ContentDialog dialog, NewDaemonConnectionInput newDaemonConnectionInput) = await ConstructConnectDaemonDialog(address, jwt);
+            dialog.PrimaryButtonClick += (o, args) =>
+            {
+                DaemonListView.Items.Remove(daemon);
+                TryConnectDaemon(
+                    address: newDaemonConnectionInput.wsEdit.Text,
+                    jwt: newDaemonConnectionInput.jwtEdit.Password
+                );
+            };
+            try
+            {
+                await dialog.ShowAsync();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+            return (o, args) => { };
+        }
+
+        private void TryConnectDaemon(string address, string jwt)
+        {
+            DaemonBorder daemon = new() { Address = address, JWT = jwt, Status = "ing" };
+            daemon.ConnectionEditButton.Click += new RoutedEventHandler(async (sender, e) => await EditDaemonConnection(sender, e, daemon.Address, daemon.JWT, daemon));
+            DaemonListView.Items.Add(daemon);
+        }
+
     }
 }
