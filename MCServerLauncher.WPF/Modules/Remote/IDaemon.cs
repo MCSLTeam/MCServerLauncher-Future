02077140bc060a6c310ad99a5db3f105b39b9d4e
AresConnor <39639716+AresConnor@users.noreply.github.com>
2024-09-02 19:58:01 +0800

    [wpf] docs: IDaemon rpc

--- MCServerLauncher.WPF/Modules/Remote/IDaemon.cs
+++ MCServerLauncher.WPF/Modules/Remote/IDaemon.cs
@@ -21,11 +21,32 @@ namespace MCServerLauncher.WPF.Modules.Remote
         DateTime LastPing { get; }
         ClientConnection Connection { get; }
 
+        /// <summary>
+        ///     rpc: 上传文件
+        /// </summary>
+        /// <param name="path">目标上传文件的本地路径</param>
+        /// <param name="dst">位于服务器上的上传目标路径</param>
+        /// <param name="chunkSize">传输分块大小</param>
+        /// <returns>上传上下文,用于查看进度,剩余时间,上传状态和取消上传等</returns>
         Task<UploadContext> UploadFileAsync(string path, string dst, int chunkSize);
 
-        // Task<(bool, long)> UploadFileChunkAsync(UploadFileToken token, long offset, byte[] data, int bytes);
+        /// <summary>
+        ///     rpc: 取消上传
+        /// </summary>
+        /// <param name="context">要取消的上传上下文</param>
+        /// <returns></returns>
         Task UploadFileCancelAsync(UploadContext context);
+
+        /// <summary>
+        ///     rpc: 获取Daemon宿主机上的Java列表
+        /// </summary>
+        /// <returns></returns>
         Task<List<JavaInfo>> GetJavaListAsync();
+
+        /// <summary>
+        ///     关闭连接
+        /// </summary>
+        /// <returns></returns>
         Task CloseAsync();
     }
 }
