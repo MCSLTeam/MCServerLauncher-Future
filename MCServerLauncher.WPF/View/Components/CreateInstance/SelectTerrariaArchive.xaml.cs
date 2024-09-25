10dc650041c0931d8b523e59f9414f60fcecee04
LxHTT <lxhtt@mcsl.com.cn>
2024-09-25 12:04:03 +0800

    [wpf] feat: terraria instance creation interface

---
+++ MCServerLauncher.WPF/View/Components/CreateInstance/SelectTerrariaArchive.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MCServerLauncher.WPF.View.Components.CreateInstance
+{
+    /// <summary>
+    ///    SelectTerrariaArchive.xaml 的交互逻辑
+    /// </summary>
+    public partial class SelectTerrariaArchive
+    {
+        public SelectTerrariaArchive()
+        {
+            InitializeComponent();
+        }
+    }
+}
