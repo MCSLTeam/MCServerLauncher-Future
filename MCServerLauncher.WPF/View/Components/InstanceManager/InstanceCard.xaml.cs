456928834f4e9799f60aa0e2605baa9f9878a322
LxHTT <lxhtt@mcsl.com.cn>
2024-11-01 20:23:35 +0800

    [wpf] feat: intance manager

--- MCServerLauncher.WPF/View/Components/InstanceManager/InstanceCard.xaml.cs
+++ MCServerLauncher.WPF/View/Components/InstanceManager/InstanceCard.xaml.cs
@@ -1,11 +1,11 @@
 ﻿namespace MCServerLauncher.WPF.View.Components.InstanceManager
 {
     /// <summary>
-    ///    InstanceBorder.xaml 的交互逻辑
+    ///    InstanceCard.xaml 的交互逻辑
     /// </summary>
-    public partial class InstanceBorder
+    public partial class InstanceCard
     {
-        public InstanceBorder()
+        public InstanceCard()
         {
             InitializeComponent();
         }
