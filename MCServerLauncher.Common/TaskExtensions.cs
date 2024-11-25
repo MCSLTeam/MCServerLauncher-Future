c2f5cdd8683073309a0c8d6858ca7a79616d1f87
Connor Zheng <2837434884@qq.com>
2024-11-25 22:11:28 +0800

    [Common] feat: add task timeout extension

---
+++ MCServerLauncher.Common/TaskExtensions.cs
@@ -0,0 +1,34 @@
+namespace MCServerLauncher.Common;
+
+public static class TaskExtensions
+{
+    public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
+    {
+        using var timeoutCancellationTokenSource = new CancellationTokenSource();
+        var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+        if (completedTask != task) throw new TimeoutException("Task has timed out.");
+        
+        timeoutCancellationTokenSource.Cancel();
+        return await task; // 传递异常
+    }
+    
+    public static Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, int millisecondsTimeout)
+    {
+        return task.TimeoutAfter(TimeSpan.FromMilliseconds(millisecondsTimeout));
+    }
+    
+    public static async Task TimeoutAfter(this Task task, TimeSpan timeout)
+    {
+        using var timeoutCancellationTokenSource = new CancellationTokenSource();
+        var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+        if (completedTask != task) throw new TimeoutException("Task has timed out.");
+        
+        timeoutCancellationTokenSource.Cancel();
+        await task; // 传递异常
+    }
+    
+    public static Task TimeoutAfter(this Task task, int millisecondsTimeout)
+    {
+        return task.TimeoutAfter(TimeSpan.FromMilliseconds(millisecondsTimeout));
+    }
+}
