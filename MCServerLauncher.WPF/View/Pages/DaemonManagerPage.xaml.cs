67555eb7553d2a20113a3d1ba5d82917560f6ef3
LxHTT <lxhtt@mcsl.com.cn>
2024-11-30 10:28:11 +0800

    [wpf] [daemon] feat: daemon list

--- MCServerLauncher.WPF/View/Pages/DaemonManagerPage.xaml.cs
+++ MCServerLauncher.WPF/View/Pages/DaemonManagerPage.xaml.cs
@@ -11,10 +11,13 @@ namespace MCServerLauncher.WPF.View.Pages
         public DaemonManagerPage()
         {
             InitializeComponent();
-            DaemonCardContainer.Items.Clear();
-            foreach (DaemonsListManager.DaemonConfigModel daemon in DaemonsListManager.DaemonList)
+            // Refresh trigger when page is visible
+            IsVisibleChanged += (s, e) =>
             {
-                DaemonCardContainer.Items.Add(new DaemonCard()
+                DaemonCardContainer.Items.Clear();
+                foreach (DaemonsListManager.DaemonConfigModel daemon in DaemonsListManager.DaemonList)
+                {
+                    DaemonCard daemonCard = new DaemonCard
                     {
                         Address = $"{(daemon.IsSecure ? "wss" : "ws")}://{daemon.EndPoint}:{daemon.Port}",
                         IsSecure = daemon.IsSecure,
@@ -23,9 +26,11 @@ namespace MCServerLauncher.WPF.View.Pages
                         Username = daemon.Username,
                         Password = daemon.Password,
                         FriendlyName = daemon.FriendlyName ?? LanguageManager.Localize["Main_DaemonManagerNavMenu"],
-                    }
-                );
-            }
+                    };
+                    DaemonCardContainer.Items.Add(daemonCard);
+                    daemonCard.ConnectDaemon();
+                }
+            };
         }
     }
 }
