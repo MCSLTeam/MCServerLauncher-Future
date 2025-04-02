0055673902910753472aa357cc990e6a3d5bd1ec
Connor Zheng <39639716+AresConnor@users.noreply.github.com>
2025-04-02 10:21:12 +0800

    ref: exclude Resource directory from daemon project

--- MCServerLauncher.Daemon/Resources/Action/gen_actions.sh
+++
@@ -1,1 +0,0 @@
-python Resources/Action/actions_meta.py --meta /Users/connorzheng/workspace/MCServerLauncher-Future/MCServerLauncher.Daemon/Resources/Action/actions_meta.yml --out /Users/connorzheng/workspace/MCServerLauncher-Future/MCServerLauncher.Daemon/Remote/Action/Actions.cs
