7f9980c19cea50ff0159b2ecbe13a8a293d90e59
Connor Zheng <39639716+AresConnor@users.noreply.github.com>
2025-04-16 18:02:47 +0800

    feat: instance command with simple process info

--- MCServerLauncher.Daemon/Console/Commands/InstanceCommand.cs
+++ MCServerLauncher.Daemon/Console/Commands/InstanceCommand.cs
@@ -1,7 +1,9 @@
 ﻿using Brigadier.NET;
 using Brigadier.NET.Builder;
 using Brigadier.NET.Tree;
+using MCServerLauncher.Common.ProtoType.Instance;
 using MCServerLauncher.Daemon.Minecraft.Server;
+using MCServerLauncher.Daemon.Utils.Status;
 
 namespace MCServerLauncher.Daemon.Console.Commands;
 
@@ -15,20 +17,40 @@ public static class InstanceCommand
             {
                 var source = cmd.Source;
                 var manager = source.GetRequiredService<IInstanceManager>();
-                foreach (var (_, instance) in manager.Instances)
-                {
-                    ShowInstanceInformation(source, instance);
-                }
+                foreach (var (_, instance) in manager.Instances) ShowInstanceInformation(source, instance).Wait();
 
                 source.SendFeedback("共 {0} 个实例", manager.Instances.Count);
                 return 0;
             })));
     }
 
-    public static void ShowInstanceInformation<TSource>(TSource source, Instance instance)
+    public static async Task ShowInstanceInformation<TSource>(TSource source, Instance instance)
         where TSource : ConsoleCommandSource
     {
         source.SendFeedback(" - {0} ({1})", instance.Config.Name, instance.Config.Uuid);
         source.SendFeedback("   - 状态: {0}", instance.Status.ToString());
+
+        if (instance.Status is InstanceStatus.Running or InstanceStatus.Starting)
+        {
+            var (mem, cpu) = await ProcessInfo.GetProcessUsageAsync(instance.ProcessId);
+            source.SendFeedback("   - PID: {0}", instance.ProcessId);
+            source.SendFeedback("   - 端口: {0}", instance.Port);
+            source.SendFeedback("   - 内存: {0}", GetMemoryString(mem));
+            source.SendFeedback("   - CPU: {0}%", cpu);
+        }
+    }
+
+    private static string GetMemoryString(double bytes)
+    {
+        if (bytes < 1024) return $"{bytes} B";
+
+        bytes = bytes / 1024;
+        if (bytes < 10240) return $"{bytes:F2} KB";
+
+        bytes = bytes / 1024;
+        if (bytes < 10240) return $"{bytes:F2} MB";
+
+        bytes = bytes / 1024;
+        return $"{bytes:F2} GB";
     }
 }
